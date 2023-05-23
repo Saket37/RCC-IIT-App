064a87d771b5de9234cd248a422ce314f47742cf
Saket37 <anandsaket37@gmail.com>
2023-05-23 15:18:20 +0530

    added full impl for the courses

--- app/src/main/java/com/example/rcciitapp/ui/courses/Courses.kt
+++ app/src/main/java/com/example/rcciitapp/ui/courses/Courses.kt
@@ -4,9 +4,16 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -50,14 +57,103 @@ fun CourseContent(
             } else if (uiState.error?.isNotBlank() == true || uiState.error?.isNotBlank() == true) {
                 Toast.makeText(context, uiState.error, Toast.LENGTH_SHORT).show()
             } else {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+
+                    ) {
+                        Text(
+                            text = "B.TECH",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp
+                        )
+                    }
+                }
                 items(uiState.courses) {
-                    CoursesCard(
-                        onNavigate = { /*TODO*/ },
-                        title = it.courseName,
-                        branch = it.branch,
-                        duration = it.duration,
-                        intake = it.intake
-                    )
+                    if (it.branch == "B.TECH")
+                        Box(
+                            modifier = Modifier
+                                .padding(6.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CoursesCard(
+                                onNavigate = { /*TODO*/ },
+                                title = it.courseName,
+                                branch = it.branch,
+                                duration = it.duration,
+                                intake = it.intake
+                            )
+                        }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Divider(modifier = Modifier.padding(24.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+
+                    ) {
+                        Text(
+                            text = "M.TECH",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp
+                        )
+                    }
+                }
+                items(uiState.courses) {
+                    if (it.branch == "M.TECH")
+                        Box(
+                            modifier = Modifier
+                                .padding(6.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CoursesCard(
+                                onNavigate = { /*TODO*/ },
+                                title = it.courseName,
+                                branch = it.branch,
+                                duration = it.duration,
+                                intake = it.intake
+                            )
+                        }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Divider(modifier = Modifier.padding(24.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "MCA",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp
+                        )
+                    }
+                }
+                items(uiState.courses) {
+                    if (it.branch == "MCA")
+                        Box(
+                            modifier = Modifier
+                                .padding(6.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CoursesCard(
+                                onNavigate = { /*TODO*/ },
+                                title = it.courseName,
+                                branch = it.branch,
+                                duration = it.duration,
+                                intake = it.intake
+                            )
+                        }
                 }
             }
         }
